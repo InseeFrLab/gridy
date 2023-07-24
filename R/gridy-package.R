8f5e210261f990064463bd000b5ce69542f34621
julienjamme <julien.jamme@insee.fr>
2023-07-24 17:24:57 +0200

    check build

--- R/gridy-package.R
+++ R/gridy-package.R
@@ -4,5 +4,9 @@
 NULL
 ## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
+#' @import data.table
 ## usethis namespace: end
 NULL
+
+## quiets concerns of R CMD check : the .'s that appear in data.table
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
